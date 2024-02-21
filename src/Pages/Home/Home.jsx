bd42acd3f1ea68c67cb96a960035c323d46a6b76
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-02-21 15:26:41 +0600

    about section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import AboutUs from "@/components/SectionComponents/AboutUS/AboutUS";
 import Banner from "@/components/SectionComponents/Banner/Banner";
 import Container from "@/components/SectionComponents/Container/Container";
 import OurServices from "@/components/SectionComponents/OurServices/OurServices";
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <>
       <Banner />
+      <AboutUs />
       <OurServices />
     </>
   );
---
+++ src/components/SectionComponents/AboutUS/AboutUS.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Container from '../Container/Container';
+import aboutUsImg from "@/assets/about/circle-unscreen.gif"
+import aboutIcon1 from "@/assets/about/Group 71042.png"
+import aboutIcon2 from "@/assets/about/Group 71043.png"
+import aboutIcon3 from "@/assets/about/Group 71043 (1).png"
+import aboutLight from "@/assets/about/Light.png"
+import aboutBall from "@/assets/about/Ball-2.png"
+
+
+const AboutUs = () => {
+  return (
+    <div className="bg-[#110A15] relative">
+      <img className='absolute mt-10 ml-10' src={aboutLight} alt="" />
+      <Container>
+        <div className='flex justify-center'>
+          <div className='w-1/2'>
+            <img className='w-[900px] mix-blend-screen' src={aboutUsImg} alt="" />
+          </div>
+
+          {/* right side */}
+
+          <div className='w-1/2'>
+            <p className='font-switzer uppercase text-[#3CBDF4] text-[18px] font-medium'>About Us</p>
+            <div className='max-w-[520px]'>
+              <h2 className='font-switzer text-[45px] font-semibold text-[#fff] w-full mb-[50px]'>Unveiling the Genesis of <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#A84EFF] to-[#7815F8]'>Terminal Labs'</span> Name</h2>
+              {/* three down card */}
+              <div>
+                <div className='flex mb-[60px]'>
+                  <div>
+                    <img src={aboutIcon1} alt="" />
+                  </div>
+                  <div className='ml-[24px]'>
+                    <h5 className='font-switzer font-medium text-[#fff] text-[22px]'>From The Terminal journey</h5>
+                    <p className='font-amulya text-[18px] text-[#B2A6B9] w-full'>In the early days of our founding, we sought a name that embodied our mission and the journey we take with our partners.</p>
+                  </div>
+                </div>
+                <div className='flex mb-[60px]'>
+                  <div>
+                    <img src={aboutIcon2} alt="" />
+                  </div>
+                  <div className='ml-[24px]'>
+                    <h5 className='font-switzer font-medium text-[#fff] text-[22px]'>Guiding Innovation's Flight in Web3</h5>
+                    <p className='font-amulya text-[18px] text-[#B2A6B9] w-full'>Terminal Labs stands as a crucial gathering point for ideas and dreams, readying projects for the journey ahead.</p>
+                  </div>
+                </div>
+                <div className='flex mb-[60px]'>
+                  <div>
+                    <img src={aboutIcon3} alt="" />
+                  </div>
+                  <div className='ml-[24px]'>
+                    <h5 className='font-switzer font-medium text-[#fff] text-[22px]'>Crafting the Ascent of Visionary Projects</h5>
+                    <p className='font-amulya text-[18px] text-[#B2A6B9] w-full'>Terminal Labs isn't just our name; it's our commitment to change and our role in shaping visionary projects.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+      <div className='flex justify-end'>
+        <img src={aboutBall} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
