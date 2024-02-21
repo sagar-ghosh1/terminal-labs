8a2aebb267658e406902e0f13667a7fab9834b70
Protap Ghosh <protapg518@gmail.com>
2024-02-21 16:31:53 +0600

    added share

--- src/components/SectionComponents/ShareWithUs/ShareWithUs.jsx
+++ src/components/SectionComponents/ShareWithUs/ShareWithUs.jsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Container from "../Container/Container";
 import { MdArrowOutward } from "react-icons/md";
+import qrCode from "@/assets/develop/qrcode.png";
 
 const ShareWithUs = () => {
   return (
@@ -8,16 +9,23 @@ const ShareWithUs = () => {
       <Container>
         <div className='p-[1px] bg-gradient-to-b from-[#70636365] from-10% to-[#6d3dfe93] to-90% rounded-2xl'>
           <div>
-            <div>
+            {/* Section Content */}
+            <div className="md:flex justify-between items-center">
               <div>
-                <h2>Got a Project Idea! Share with us for develop!</h2>
-                <p>Got a project idea you're excited about? Share it with us today and let's make it happen together</p>
+                <p className="text-white text-3xl md:text-4xl xl:text-4xl font-semibold font-switzer  md:w-[490px] xl:max-w-[475px]">Got a Project Idea! Share with us for develop!</p>
+                <p className="text-[#B2A6B9] font-amulya xl:max-w-[415px]">Got a project idea you're excited about? Share it with us today and let's make it happen together</p>
+
                 <Button className="rounded-[8px] group cursor-pointer px-5 md:px-7 border border-[#a74fff4f]">
                   <span className="flex justify-center items-center">
                     <p className='text-[15px] font-amulya'>Contact</p>
                     <MdArrowOutward className='text-[17px] ml-2 group-hover:-mt-2 duration-300' />
                   </span>
                 </Button>
+              </div>
+
+              {/* Section Image */}
+              <div>
+                <img src={qrCode} alt="QR Code" className="md:w-[300px]" />
               </div>
             </div>
           </div>
