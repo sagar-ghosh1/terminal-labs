11453fd0f5d2381e8a4162365b03c9b815616a5c
Protap Ghosh <protapg518@gmail.com>
2024-02-23 09:18:22 +0600

    fixed responsive issue

--- src/components/SectionComponents/ShareWithUs/ShareWithUs.jsx
+++ src/components/SectionComponents/ShareWithUs/ShareWithUs.jsx
@@ -7,45 +7,51 @@ import circleShadow from '@/assets/develop/lightBg.png';
 
 const ShareWithUs = () => {
   return (
-    <div className="bg-[#180c21] relative pt-36 pb-14">
+    <div className="bg-[#180c21] relative pb-14 overflow-hidden">
+
       <Container>
         <div>
-          <div className="z-10">
+          {/* <div className="">
             <img src={circleShadow} alt="Shadow" className="w-[290px] absolute right-0 top-0 blur-[4px]" />
-          </div>
+          </div> */}
 
           {/* QR Code card */}
-          <div className=" z-0">
-            <div className='p-[1px] bg-gradient-to-r from-[#6f359f94] from-10% to-[#261336] to-90% rounded-2xl'>
-              <div className="bg-gradient-to-r from-[#1e1425] from-10% to-[#251531] to-90% rounded-2xl">
-                <div style={{ backgroundImage: `url(${patternBg})` }} className="bg-no-repeat bg-bottom bg-contain">
-                  <div className="bg-[#261233] bg-opacity-75 rounded-2xl">
-                    {/* Section Content */}
-                    <div className="md:flex justify-between items-center py-6 px-8 md:py-9 md:px-12 xl:py-12 xl:px-16">
-                      <div>
-                        <div className="text-white text-3xl md:text-4xl xl:text-4xl font-semibold font-switzer md:w-[490px] xl:max-w-[475px]">
-                          <p className="mb-2">Got a Project Idea! Share</p>
-                          <p>with us for develop!</p>
+          <div>
+            <div className="object-contain h-40 w-full">
+              <div style={{ backgroundImage: `url(${circleShadow})` }} className="bg-no-repeat w-full h-60 ml-[170px] xl:ml-[880px] 2xl:ml-[70rem]"/>
+            </div>
+            <div className="p-[1px] bg-gradient-to-r from-[#6f359f94] from-10% to-[#261336] to-90% rounded-2xl">
+              <div className='bg-gradient-to-r from-[#1e1425] from-10% to-[#251531] to-90% rounded-2xl'>
+                <div className="">
+                  <div style={{ backgroundImage: `url(${patternBg})` }} className="bg-no-repeat bg-bottom bg-contain">
+                    <div className="bg-[#261233] bg-opacity-75 rounded-2xl">
+
+                      <div className="md:flex justify-between items-center py-6 px-8 md:py-9 md:px-12 xl:py-12 xl:px-16">
+                        <div>
+                          <div className="text-white text-3xl md:text-4xl xl:text-4xl font-semibold font-switzer md:w-[490px] xl:max-w-[475px]">
+                            <p className="mb-2">Got a Project Idea! Share</p>
+                            <p>with us for develop!</p>
+                          </div>
+                          <p className="text-[#B2A6B9] font-amulya md:w-[330px] xl:w-[415px] 2xl:w-[420px] mt-5 mb-7">Got a project idea you're excited about? Share it with us today and let's make it happen together</p>
+
+                          <Button className="rounded-[8px] group cursor-pointer px-5 md:px-7 border border-[#a74fff4f]">
+                            <span className="flex justify-center items-center">
+                              <p className='text-[15px] font-amulya'>Contact</p>
+                              <MdArrowOutward className='text-[17px] ml-2 group-hover:-mt-2 duration-300' />
+                            </span>
+                          </Button>
                         </div>
-                        <p className="text-[#B2A6B9] font-amulya md:w-[330px] xl:w-[415px] 2xl:w-[420px] mt-5 mb-7">Got a project idea you're excited about? Share it with us today and let's make it happen together</p>
 
-                        <Button className="rounded-[8px] group cursor-pointer px-5 md:px-7 border border-[#a74fff4f]">
-                          <span className="flex justify-center items-center">
-                            <p className='text-[15px] font-amulya'>Contact</p>
-                            <MdArrowOutward className='text-[17px] ml-2 group-hover:-mt-2 duration-300' />
-                          </span>
-                        </Button>
-                      </div>
 
-                      {/* Section Image */}
-                      <div className="mt-14 md:mt-0">
-                        <img src={qrCode} alt="QR Code" className="md:w-[300px]" />
+                        <div className="mt-14 md:mt-0">
+                          <img src={qrCode} alt="QR Code" className="md:w-[300px]" />
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div >
+              </div >
+            </div>
           </div>
         </div>
       </Container >
