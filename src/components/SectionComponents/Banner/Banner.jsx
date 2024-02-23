733e64fa48317da1c35a30f3ad4f5ea07fb384a2
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-02-24 01:56:47 +0600

    banner section animation

--- src/components/SectionComponents/Banner/Banner.jsx
+++ src/components/SectionComponents/Banner/Banner.jsx
@@ -35,17 +35,17 @@ const Banner = () => {
           <Container>
             <div className='flex flex-col-reverse md:flex-row justify-between items-center gap-x-8 md:py-[185px]'>
               <div className='md:w-1/2 mt-20 mb-14 md:mb-0 md:mt-0'>
-                <p className='text-[15px] md:text-[16px] text-[#3CBDF4] font-switzer uppercase mb-2'>Web3 Visionary Success Journey</p>
+                <p className='text-[15px] md:text-[16px] text-[#3CBDF4] font-switzer uppercase mb-2' data-aos="fade-down" data-aos-duration="1000">Web3 Visionary Success Journey</p>
                 <div>
                   <div className='flex items-center'>
-                    <p className='text-4xl md:text-5xl xl:text-6xl font-bold font-switzer text-transparent bg-clip-text bg-gradient-to-r from-[#A84EFF] to-[#7815F8] mr-6 py-3'>Web3 Mastery</p>
+                    <p className='text-4xl md:text-5xl xl:text-6xl font-bold font-switzer text-transparent bg-clip-text bg-gradient-to-r from-[#A84EFF] to-[#7815F8] mr-6 py-3' data-aos="fade-up" data-aos-duration="1400">Web3 Mastery</p>
                     <img src={wavingHand} alt="wavingHand" className='w-10' />
                   </div>
-                  <p className='text-4xl md:text-5xl xl:text-6xl text-white font-bold font-switzer mt-1'>with Terminal Labs</p>
+                  <p className='text-4xl md:text-5xl xl:text-6xl text-white font-bold font-switzer mt-1' data-aos="fade-down" data-aos-duration="1600">with Terminal Labs</p>
                 </div>
 
-                <p className='text-[17px] text-[#B2A6B9] md:w-[70%] xl:w-[75%] my-7'>Terminal Labs is at the forefront of shaping visionary success in the Web3 landscape.</p>
-                <Button className="flex justify-center items-center rounded-[8px] group cursor-pointer px-5 md:px-7 border border-[#a74fff4f]">
+                <p className='text-[17px] text-[#B2A6B9] md:w-[70%] xl:w-[75%] my-7' data-aos="fade-up" data-aos-duration="1800">Terminal Labs is at the forefront of shaping visionary success in the Web3 landscape.</p>
+                <Button className="flex justify-center items-center rounded-[8px] group cursor-pointer px-5 md:px-7 border border-[#a74fff4f]" data-aos="fade-down" data-aos-duration="2000">
                   <span className='text-[15px] font-amulya'>See Partners</span>
                   <MdArrowOutward className='text-[17px] ml-2 group-hover:-mt-2 duration-300' />
                 </Button>
